51231b73d75a91cfa7db1f3d2f72af2872b7e6e7
Nick Maludy <nmaludy@gmail.com>
2020-07-01 08:56:45 -0400

    More work on acceptance tests

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -4,32 +4,32 @@ describe 'erlang init:' do
   case fact('os.family')
   when 'RedHat'
     default_repo_source = 'packagecloud'
-    repo_source_list = %w[bintray epel packagecloud erlang_solutions]
+    repo_source_list = %w[bintray erlang_solutions packagecloud]
   when 'Debian'
     default_repo_source = 'bintray'
-    repo_source_list = %w[bintray epel erlang_solutions]
-  end
-
-  context 'default class declaration' do
-    let(:pp) do
-      <<-EOS
-      class { 'erlang': }
-      EOS
-    end
-
-    it_behaves_like 'an idempotent resource'
-
-    describe package('erlang') do
-      it { is_expected.to be_installed }
-    end
-    describe yumrepo("erlang-#{default_repo_source}") do
-      it { is_expected.to exist }
-      it { is_expected.to be_enabled }
-    end
+    repo_source_list = %w[bintray erlang_solutions]
   end
 
   case fact('os.family')
   when 'RedHat'
+    context 'default class declaration' do
+      let(:pp) do
+        <<-EOS
+        class { 'erlang': }
+        EOS
+      end
+
+      it_behaves_like 'an idempotent resource'
+
+      describe package('erlang') do
+        it { is_expected.to be_installed }
+      end
+      describe yumrepo("erlang-#{default_repo_source}") do
+        it { is_expected.to exist }
+        it { is_expected.to be_enabled }
+      end
+    end
+
     repo_source_list.each do |repo_source|
       context "with repo source set to #{repo_source}" do
         let(:pp) do
@@ -69,6 +69,20 @@ describe 'erlang init:' do
       end
     end
   when 'Debian'
+    context 'default class declaration' do
+      let(:pp) do
+        <<-EOS
+        class { 'erlang': }
+        EOS
+      end
+
+      it_behaves_like 'an idempotent resource'
+
+      describe package('erlang') do
+        it { is_expected.to be_installed }
+      end
+    end
+
     repo_source_list.each do |repo_source|
       context "with repo source set to #{repo_source}" do
         let(:pp) do
