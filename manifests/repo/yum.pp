71617fd15ced33b300bfc67e22f5a65a4ae55d08
zilchms <zilchmarkus@web.de>
2025-03-15 15:45:55 +0100

    remove one layer of indirection from yum repo handling

--- manifests/repo/yum.pp
+++ manifests/repo/yum.pp
@@ -2,18 +2,43 @@
 class erlang::repo::yum (
   String $ensure = $erlang::repo_ensure,
   Erlang::RepoSource $source = $erlang::repo_source,
+  String $erlang_solutions_baseurl = "https://packages.erlang-solutions.com/rpm/centos/\$releasever/\$basearch",
+  String $erlang_solutions_gpgkey = 'https://packages.erlang-solutions.com/rpm/erlang_solutions.asc',
+  String $packagecloud_baseurl = "https://packagecloud.io/rabbitmq/erlang/el/${$facts['os']['release']['major']}/\$basearch",
+  String $packagecloud_gpgkey = 'https://packagecloud.io/rabbitmq/erlang/gpgkey'
 ) inherits erlang {
   case $source {
     'epel': {
-      contain erlang::repo::yum::epel
+      # include epel instead of contain because lots of other stuff declares epel
+      include epel
+      # specifically declare dependency on epel repo
+      Class['epel'] -> Package[$erlang::package_name]
     }
     'erlang_solutions': {
       # erlang_solutions package requires things from EPEL
-      contain erlang::repo::yum::epel
-      contain erlang::repo::yum::erlang_solutions
+      # include epel instead of contain because lots of other stuff declares epel
+      include epel
+      # specifically declare dependency on epel repo
+      Class['epel'] -> Package[$erlang::package_name]
+      yumrepo { 'erlang-erlang_solutions':
+        ensure   => $ensure,
+        name     => 'erlang-erlang_solutions',
+        baseurl  => $erlang_solutions_baseurl,
+        enabled  => '1',
+        gpgcheck => '1',
+        gpgkey   => $erlang_solutions_gpgkey,
+      }
     }
     'packagecloud': {
-      contain erlang::repo::yum::packagecloud
+      yumrepo { 'erlang-packagecloud':
+        ensure        => $ensure,
+        name          => 'erlang-packagecloud',
+        baseurl       => $packagecloud_baseurl,
+        enabled       => '1',
+        gpgcheck      => '0',
+        repo_gpgcheck => '1',
+        gpgkey        => $packagecloud_gpgkey,
+      }
     }
     default: {
       fail("Only 'packagecloud', 'epel' repo_source are supported for Yum repos, the following is not supported: ${source}")
--- spec/classes/repo/yum_spec.rb
+++ spec/classes/repo/yum_spec.rb
@@ -12,26 +12,60 @@ describe 'erlang::repo::yum' do # rubocop:disable RSpec/EmptyExampleGroup
         it { is_expected.to compile.with_all_deps }
 
         context 'with default parameters' do
-          it { is_expected.to contain_class('erlang::repo::yum::packagecloud') }
+          it do
+            is_expected.to contain_yumrepo('erlang-packagecloud').
+              with('ensure' => 'present',
+                'name' => 'erlang-packagecloud',
+                'baseurl' => "https://packagecloud.io/rabbitmq/erlang/el/#{facts[:os]['release']['major']}/$basearch",
+                'enabled' => '1',
+                'gpgcheck' => '0',
+                'repo_gpgcheck' => '1',
+                'gpgkey' => 'https://packagecloud.io/rabbitmq/erlang/gpgkey')
+          end
         end
 
         context 'with source set to epel' do
           let(:params) { { source: 'epel' } }
 
-          it { is_expected.to contain_class('erlang::repo::yum::epel') }
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('epel').that_comes_before('Package[erlang]') }
         end
 
         context 'with source set to erlang_solutions' do
           let(:params) { { source: 'erlang_solutions' } }
 
-          it { is_expected.to contain_class('erlang::repo::yum::epel') }
-          it { is_expected.to contain_class('erlang::repo::yum::erlang_solutions') }
+          it { is_expected.to compile.with_all_deps }
+
+          context 'with default parameters' do
+            it do
+              is_expected.to contain_yumrepo('erlang-erlang_solutions').
+                with('ensure' => 'present',
+                  'name' => 'erlang-erlang_solutions',
+                  'baseurl' => 'https://packages.erlang-solutions.com/rpm/centos/$releasever/$basearch',
+                  'enabled' => '1',
+                  'gpgcheck' => '1',
+                  'gpgkey' => 'https://packages.erlang-solutions.com/rpm/erlang_solutions.asc')
+            end
+          end
         end
 
         context 'with source set to packagecloud' do
           let(:params) { { source: 'packagecloud' } }
 
-          it { is_expected.to contain_class('erlang::repo::yum::packagecloud') }
+          it { is_expected.to compile.with_all_deps }
+
+          context 'with default parameters' do
+            it do
+              is_expected.to contain_yumrepo('erlang-packagecloud').
+                with('ensure' => 'present',
+                  'name' => 'erlang-packagecloud',
+                  'baseurl' => "https://packagecloud.io/rabbitmq/erlang/el/#{facts[:os]['release']['major']}/$basearch",
+                  'enabled' => '1',
+                  'gpgcheck' => '0',
+                  'repo_gpgcheck' => '1',
+                  'gpgkey' => 'https://packagecloud.io/rabbitmq/erlang/gpgkey')
+            end
+          end
         end
 
         context 'with source set to invalid' do
