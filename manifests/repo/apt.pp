baf586c73f6e2650ee11dc9cf52692efc2c5a5d5
zilchms <zilchmarkus@web.de>
2025-03-15 14:50:00 +0100

    unwrap one level of indirection in apt package classes

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -2,10 +2,34 @@
 class erlang::repo::apt (
   String  $ensure = $erlang::repo_ensure,
   Erlang::RepoSource $source = $erlang::repo_source,
+  String $location    = 'https://packages.erlang-solutions.com/debian',
+  # trusty, xenial, bionic, etc
+  String $release     = downcase($facts['os']['distro']['codename']),
+  String $repos       = 'contrib',
+  String $key         = '434975BD900CCBE4F7EE1B1ED208507CA14F4FCA',
+  String $key_source  = 'https://packages.erlang-solutions.com/debian/erlang_solutions.asc',
+  Optional[Variant[Numeric, String]] $pin = $erlang::package_apt_pin,
 ) inherits erlang {
   case $source {
     'erlang_solutions': {
-      contain erlang::repo::apt::erlang_solutions
+      apt::source { 'erlang-erlang_solutions':
+        ensure   => $ensure,
+        location => $location,
+        release  => $release,
+        repos    => $repos,
+        key      => {
+          'id'     => $key,
+          'source' => $key_source,
+        },
+      }
+
+      if $pin {
+        apt::pin { 'erlang':
+          packages => '*',
+          priority => $pin,
+          origin   => inline_template('<%= require \'uri\'; URI(@location).host %>'),
+        }
+      }
     }
     default: {
       fail("Only 'erlang_solutions' repo_sources is supported for Apt repos, the following is not supported: ${source}")
--- spec/classes/repo/apt_spec.rb
+++ spec/classes/repo/apt_spec.rb
@@ -13,8 +13,19 @@ describe 'erlang::repo::apt' do # rubocop:disable RSpec/EmptyExampleGroup
 
         context 'with source set to erlang_solutions' do
           let(:params) { { source: 'erlang_solutions' } }
+          let(:release) { facts[:os]['distro']['codename'] }
 
-          it { is_expected.to contain_class('erlang::repo::apt::erlang_solutions') }
+          it do
+            is_expected.to contain_apt__source('erlang-erlang_solutions').
+              with('ensure' => 'present',
+                'location' => 'https://packages.erlang-solutions.com/debian',
+                'release' => release,
+                'repos' => 'contrib',
+                'key' => {
+                'id' => '434975BD900CCBE4F7EE1B1ED208507CA14F4FCA',
+                'source' => 'https://packages.erlang-solutions.com/debian/erlang_solutions.asc'
+            })
+          end
         end
 
         context 'with source set to invalid' do
